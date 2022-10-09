9f9a582f1addd0d207892adae261b01773c7a5de
adeebshakri_ <adeebshakri7@gmail.com>
2022-10-09 21:23:58 +0530

    Updates

---
+++ precache-manifest.3b3a1ffa293a67bf9d78109eccecb45d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3e938b3a379434339f3",
+    "url": "/adeebshakri/static/css/main.fe08e1f5.chunk.css"
+  },
+  {
+    "revision": "e3e938b3a379434339f3",
+    "url": "/adeebshakri/static/js/main.e3e938b3.chunk.js"
+  },
+  {
+    "revision": "07fb01a1dae44c8e0739",
+    "url": "/adeebshakri/static/css/1.286be1a3.chunk.css"
+  },
+  {
+    "revision": "07fb01a1dae44c8e0739",
+    "url": "/adeebshakri/static/js/1.07fb01a1.chunk.js"
+  },
+  {
+    "revision": "92c4b30f012f5ee231e3",
+    "url": "/adeebshakri/static/js/runtime~main.92c4b30f.js"
+  },
+  {
+    "revision": "0963a443dd80662ff30b17b668508c76",
+    "url": "/adeebshakri/static/media/cv.0963a443.pdf"
+  },
+  {
+    "revision": "690851a6170cd913e8a04d5ac46a108f",
+    "url": "/adeebshakri/static/media/me.690851a6.png"
+  },
+  {
+    "revision": "9086731f1fc56e6ee4422a00a6813f4f",
+    "url": "/adeebshakri/static/media/me-about.9086731f.jpg"
+  },
+  {
+    "revision": "d1e8fb803162800c55cbc18b57899262",
+    "url": "/adeebshakri/static/media/portfolio1.d1e8fb80.jpg"
+  },
+  {
+    "revision": "13db5e2202aa39b783fded70d877b662",
+    "url": "/adeebshakri/static/media/portfolio2.13db5e22.jpg"
+  },
+  {
+    "revision": "ce79413f3dfb4b9ab668d3236a4a04d1",
+    "url": "/adeebshakri/static/media/portfolio3.ce79413f.jpg"
+  },
+  {
+    "revision": "04dbe3ab7df1162d6c760d973af4ad6a",
+    "url": "/adeebshakri/static/media/portfolio4.04dbe3ab.jpg"
+  },
+  {
+    "revision": "c6e34db9e36b84eb604b3d8897e2b5c1",
+    "url": "/adeebshakri/static/media/portfolio5.c6e34db9.png"
+  },
+  {
+    "revision": "32a902840cca71090c80890561f4be7f",
+    "url": "/adeebshakri/static/media/portfolio6.32a90284.jpg"
+  },
+  {
+    "revision": "2de04724c47f8d6896789153ac328078",
+    "url": "/adeebshakri/static/media/bg-texture.2de04724.png"
+  },
+  {
+    "revision": "c2a933efcd8c3fcc819f04e971fd0d4e",
+    "url": "/adeebshakri/index.html"
+  }
+];
